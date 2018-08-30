e080dd5f24fceb08c92daa7620baaa36d7b1bb58
jwuestef <jwuestef@gmail.com>
2018-08-30 08:00:50 -0400

    final status

---
+++ FluentApiSection/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace FluentApiSection.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FluentApiSection.PlutoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FluentApiSection.PlutoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
